e80e177c1d6b0cfc6de6de2a88e4273b9c762db7
Stefan Kopieczek <stefankopieczek@gmail.com>
2023-09-15 19:59:34 +0100

    Get linting working

--- src/bitutils/onesComplement.ts
+++ src/bitutils/onesComplement.ts
@@ -1,7 +1,7 @@
-export function signExtend(original: number): number {
-    // Set the 16th bit equal to the value of the 15th bit.
-    // This is used by overflow-aware calculations, as if overflow onto the 16th bit occurs
-    // it will then differ from the 15th bit.
-    // Thus we initially set them equal prior to such calculations, so that if they ever differ we will know overflow occurred.
-    return (original & 0o77777) | ((original << 1) & 0o100000)
+export function signExtend (original: number): number {
+  // Set the 16th bit equal to the value of the 15th bit.
+  // This is used by overflow-aware calculations, as if overflow onto the 16th bit occurs
+  // it will then differ from the 15th bit.
+  // Thus we initially set them equal prior to such calculations, so that if they ever differ we will know overflow occurred.
+  return (original & 0o77777) | ((original << 1) & 0o100000)
 }
--- src/hardware/cpu/instructions.ts
+++ src/hardware/cpu/instructions.ts
@@ -1,10 +1,9 @@
-import { signExtend } from "../../bitutils/onesComplement";
-import { Memory, MemoryRef } from "../memory/memory";
+import { type Memory, type MemoryRef } from '../memory/memory'
 
-export function ad(memory: Memory, operandAddress: MemoryRef): void {    
-    const left = memory.registers.A & 0o77777
-    const right = memory.read(operandAddress)    
-    const interim = left + right
-    const result = (interim & 0o77777) + (interim >>> 15)
-    memory.registers.A = (memory.registers.A & 0o100000) | result
+export function ad (memory: Memory, operandAddress: MemoryRef): void {
+  const left = memory.registers.A & 0o77777
+  const right = memory.read(operandAddress)
+  const interim = left + right
+  const result = (interim & 0o77777) + (interim >>> 15)
+  memory.registers.A = (memory.registers.A & 0o100000) | result
 }
